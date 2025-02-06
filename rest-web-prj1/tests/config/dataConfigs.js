c69462d16447b246ef448833bbc2386308a723e4
Nitesh <niteshprimedev@gmail.com>
2025-02-06 07:00:58 +0530

    Rest & Web Playwright Prj1

---
+++ rest-web-prj1/tests/config/dataConfigs.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const dataConfigs = {
+    restApiConfigs:{
+        baseURL: `${process.env.BASEURL}`,
+        postConfigs: {
+            resource: `${process.env.POST_RESC}`,
+            params: `?key=${process.env.KEY}`,
+            bodyPayload: {
+                "location": {
+                "lat": "-38.383494",
+                "lng": 33.427362
+                },
+                "accuracy": 50,
+                
+                "name": "Frontline house",
+                "phone_number": "(+91) 983 893 3937",
+                "address": "29, side layout, cohen 09",
+                "types": [
+                "shoe park",
+                "shop"
+                ],
+                "website": "http://google.com",
+                "language": "French-IN"
+            },
+        },
+        deleteConfigs: {
+            resource:  `${process.env.DELETE_RESC}`,
+            params: `?key=${process.env.KEY}`,
+            bodyPayload: { "place_id": process.env.PLACE_ID }, // hard coded place_id 
+            // & to be updated in the delete test case;
+        },
+        getConfigs: {
+            resource:  `${process.env.GET_RESC}`,
+            params: `&key=${process.env.KEY}`,
+            place_id: process.env.PLACE_ID // hard coded place_id 
+            // & to be updated in the delete test case;
+        },
+        putConfigs: {
+            resource:  `${process.env.PUT_RESC}`,
+            params: `&key=${process.env.KEY}`,
+            bodyPayload: { "place_id": process.env.PLACE_ID, "address": "70 Summer walk, USA", "key": process.env.KEY },
+            // hard coded place_id 
+            // & to be updated in the delete test case;
+        }
+    },
+    webUIConfigs:{
+
+    }
+};
+
+module.exports = { dataConfigs };
---
+++ rest-web-prj1/tests/pages/APIAutomation.js
@@ -0,0 +1,19 @@
+class APIAutomation {
+    constructor(baseURL, expect, requestCtx){
+        this.baseURL = baseURL;
+        this.expect = expect;
+        this.requestCtx = requestCtx;
+    }
+    
+    // Making a GET Request;
+    
+    
+    // Making a POST Request;
+
+
+    // Making a PUT Request;
+
+    
+    // Making a DELETE Request;
+
+}
